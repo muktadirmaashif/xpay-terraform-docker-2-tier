4d29e7dbc46985577e31540ce5483ecce6964cca
muktadirmaashif <muktadirmaashif@gmail.com>
2025-01-26 01:23:27 +0600

    cleaning configs

--- api.tf
+++ api.tf
@@ -3,19 +3,6 @@ resource "docker_container" "xpay" {
   name  = "${var.api_container_name}_${count.index + 1}"
   image = var.api_image_name
 
-  command = [
-    "/bin/sh",
-    "-c",
-    <<-EOT
-            while true; do
-              if ! pg_isready -h ${var.pg_hostname} -p ${var.pg_port}; then
-                export DB_PORT=${var.pg_port + 1}
-              fi
-              sleep 5
-            done &
-          EOT
-  ]
-
   env = [
     "DB_URL=${var.pg_hostname}://${var.pg_user}:${var.pg_password}@${var.pg_hostname}:${var.pg_port}/${var.pg_db_name}?sslmode=disable&timezone=UTC",
     "SERVER_ADDRESS=${var.api_server_addr}",
@@ -34,11 +21,5 @@ resource "docker_container" "xpay" {
     docker_container.postgres_1,
     docker_container.postgres_2
   ]
-  healthcheck {
-    test     = ["CMD-SHELL", "curl -f ${var.pg_hostname}:${var.pg_port}"]
-    interval = "5s"
-    timeout  = "3s"
-    retries  = 3
-  }
-  restart = "always"
+  restart = "unless-stopped"
 }
